bc7556923ae857ea17a4c78779bbb82d1740cb9d
sviat.stoliarenko <sviat.stoliarenko@bigcommerce.com>
2023-08-27 17:04:14 +0300

    add more abstraction for readability

--- src/main/scala/java/lang/ThreadLocalMapExtractor.scala
+++ src/main/scala/java/lang/ThreadLocalMapExtractor.scala
@@ -2,6 +2,61 @@ import java.util.concurrent.Executors
 
 object ThreadLocalMapExtractor extends App {
 
+  class ThreadLocalMapWrapper private(threadLocalMap: Any) {
+    import ThreadLocalMapWrapper._
+
+    def set(key: AnyRef, value: AnyRef) = {
+      setThreadLocalMapMethod.invoke(threadLocalMap, key, value)
+    }
+
+    def set(entryWrapper: EntryWrapper) = {
+      setThreadLocalMapMethod.invoke(threadLocalMap, entryWrapper.key, entryWrapper.value)
+    }
+
+    def value = threadLocalMap
+
+  }
+
+  object ThreadLocalMapWrapper {
+
+    // TODO types for parameters
+    def apply(key: AnyRef, value: AnyRef): ThreadLocalMapWrapper = {
+      new ThreadLocalMapWrapper(createWithFirstElementMapMethod.newInstance(key, value))
+    }
+
+    def apply(entryWrapper: EntryWrapper): ThreadLocalMapWrapper = ThreadLocalMapWrapper(entryWrapper.key, entryWrapper.value)
+
+    val threadLocalClazz = Class.forName("java.lang.ThreadLocal")
+    val threadLocalMapClazz = Class.forName("java.lang.ThreadLocal$ThreadLocalMap")
+    val createInheritedMapMethod = threadLocalMapClazz.getDeclaredConstructor(threadLocalMapClazz)
+    createInheritedMapMethod.setAccessible(true)
+    val createWithFirstElementMapMethod = threadLocalMapClazz.getDeclaredConstructor(threadLocalClazz, classOf[Object])
+    createWithFirstElementMapMethod.setAccessible(true)
+    println(threadLocalMapClazz.getDeclaredMethods.filter(_.getName == "set").mkString(", "))
+    val setThreadLocalMapMethod = threadLocalMapClazz.getDeclaredMethod("set", threadLocalClazz, classOf[Object])
+
+    val getTableThreadMapMethod = threadLocalMapClazz.getDeclaredField("table")
+    getTableThreadMapMethod.setAccessible(true)
+  }
+
+  class EntryWrapper(entry: Any) {
+    import EntryWrapper._
+    def key = entityKeyMethod.invoke(entry)
+    def value = entityValueMethod.get(entry)
+  }
+
+  object EntryWrapper {
+    def apply(entry: Any) = new EntryWrapper(entry)
+
+    val entityClazz = Class.forName("java.lang.ThreadLocal$ThreadLocalMap$Entry")
+//    println(entityClazz.getMethods.mkString(", "))
+//    println(entityClazz.getMethods.filter(_.getName == "get").mkString(", "))
+    val entityKeyMethod = entityClazz.getMethod("get")
+    entityKeyMethod.setAccessible(true)
+    val entityValueMethod = entityClazz.getDeclaredField("value")
+    entityValueMethod.setAccessible(true)
+  }
+
 
   val s: ThreadLocal[String] = new ThreadLocal[String]()
 
@@ -9,29 +64,28 @@ object ThreadLocalMapExtractor extends App {
   val threadLocalsField = threadClazz.getDeclaredField("threadLocals")
   threadLocalsField.setAccessible(true)
   println(threadLocalsField)
-//  f
 
 //  clazz.getDeclaredFields.foreach(println(_))
 
   val threadLocalClazz = Class.forName("java.lang.ThreadLocal$ThreadLocalMap")
-//  println(threadLocalClazz.getDeclaredMethods.mkString(", "))
-  val createInheritedMapMethod = threadLocalClazz.getDeclaredConstructor(threadLocalClazz)
-  createInheritedMapMethod.setAccessible(true)
-  val createWithFirstElementMapMethod = threadLocalClazz.getDeclaredConstructor(Class.forName("java.lang.ThreadLocal"), classOf[Object])
-  createWithFirstElementMapMethod.setAccessible(true)
-  println(threadLocalClazz.getDeclaredMethods.filter(_.getName == "set").mkString(", "))
-  val setThreadLocalMapMethod = threadLocalClazz.getDeclaredMethod("set", Class.forName("java.lang.ThreadLocal"), classOf[Object])
-
+////  println(threadLocalClazz.getDeclaredMethods.mkString(", "))
+//  val createInheritedMapMethod = threadLocalClazz.getDeclaredConstructor(threadLocalClazz)
+//  createInheritedMapMethod.setAccessible(true)
+//  val createWithFirstElementMapMethod = threadLocalClazz.getDeclaredConstructor(Class.forName("java.lang.ThreadLocal"), classOf[Object])
+//  createWithFirstElementMapMethod.setAccessible(true)
+//  println(threadLocalClazz.getDeclaredMethods.filter(_.getName == "set").mkString(", "))
+//  val setThreadLocalMapMethod = threadLocalClazz.getDeclaredMethod("set", Class.forName("java.lang.ThreadLocal"), classOf[Object])
+//
   val getTableThreadMapMethod = threadLocalClazz.getDeclaredField("table")
   getTableThreadMapMethod.setAccessible(true)
 
-  val entityClazz = Class.forName("java.lang.ThreadLocal$ThreadLocalMap$Entry")
-  println(entityClazz.getMethods.mkString(", "))
-  println(entityClazz.getMethods.filter(_.getName == "get").mkString(", "))
-  val entityKetMethod = entityClazz.getMethod("get")
-  entityKetMethod.setAccessible(true)
-  val entityValueMethod = entityClazz.getDeclaredField("value")
-  entityValueMethod.setAccessible(true)
+//  val entityClazz = Class.forName("java.lang.ThreadLocal$ThreadLocalMap$Entry")
+//  println(entityClazz.getMethods.mkString(", "))
+//  println(entityClazz.getMethods.filter(_.getName == "get").mkString(", "))
+//  val entityKetMethod = entityClazz.getMethod("get")
+//  entityKetMethod.setAccessible(true)
+//  val entityValueMethod = entityClazz.getDeclaredField("value")
+//  entityValueMethod.setAccessible(true)
 
   // TODO: reimplement copy. Right now InheritableThreadLocal used for test porpuses
   def copyThreadLocalMap(tlm: Object) = {
@@ -39,13 +93,15 @@ object ThreadLocalMapExtractor extends App {
     else {
       val entityArr = getTableThreadMapMethod.get(tlm).asInstanceOf[Array[_]]
       val notNullEntities = entityArr.filter(_ != null)
-      val tlmCopy = if (notNullEntities.nonEmpty) {
-        val head = notNullEntities.head
+      val tlmCopy = /*if (notNullEntities.nonEmpty)*/ {
+        val head = new EntryWrapper(notNullEntities.head)
         val tail = notNullEntities.tail
-        val newInstance = createWithFirstElementMapMethod.newInstance(entityKetMethod.invoke(head), entityValueMethod.get(head))
-        tail.foreach(entry => setThreadLocalMapMethod.invoke(newInstance, entityKetMethod.invoke(entry), entityValueMethod.get(entry)))
-        newInstance
-      } else null
+//        val newInstance =  createWithFirstElementMapMethod.newInstance(head.key, head.value)
+        val newInstance: ThreadLocalMapWrapper = ThreadLocalMapWrapper(head)
+//        tail.map(new EntryWrapper(_)).foreach(entry => setThreadLocalMapMethod.invoke(newInstance, entry.key, entry.value))
+        tail.map(new EntryWrapper(_)).foreach(entry => newInstance.set(entry))
+        newInstance.value
+      } // else null
 //      val copy = createInheritedMapMethod.newInstance(tlm)
       println(s"tlm copy $tlm")
       tlmCopy
